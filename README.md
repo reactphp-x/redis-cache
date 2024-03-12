8ebf911e5eb4b64884bf77dea0a2c26367e18c89
wpjscc <wpjscc@gmail.com>
2024-03-12 16:51:08 +0800

    first

---
+++ README.md
@@ -0,0 +1,27 @@
+# reactphp-framework-redis-cache
+
+## install
+
+```
+composer require reactphp-framework/redis-cache -vvv
+```
+
+
+## Usage
+
+```php
+use Clue\React\Redis\RedisClient;
+use Reactphp\Framework\RedisCache\RedisCache;
+$redis = new RedisClient('redis://:h%40llo@localhost');
+// $redis = new RedisClient('redis://ignored:h%40llo@localhost');
+// $redis = new RedisClient('redis://localhost?password=h%40llo');
+// $redis = new RedisClient('redis://localhost/2');
+// $redis = new RedisClient('redis://localhost?db=2');
+
+$redisCache = new RedisCache($redis);
+```
+
+see [CacheInterface](https://github.com/reactphp/cache?tab=readme-ov-file#usage)
+
+## License
+MIT
---
+++ src/RedisCache.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Reactphp\Framework\RedisCache;
+
+use React\Cache\CacheInterface;
+
+class RedisCache implements CacheInterface
+{
+    private $redis;
+
+    public function __construct($redis)
+    {
+        $this->redis = $redis;
+    }
+
+    public function get($key, $default = null)
+    {
+        return $this->redis->get($key)->then(function ($value) use ($default) {
+            return $value ?? $default;
+        }, function ($e) use ($default) {
+            return $default;
+        });
+    }
+
+    public function getMultiple($keys, $default = null)
+    {
+        return $this->redis->mget(...$keys)->then(function ($values) use ($keys, $default) {
+            $result = [];
+            foreach ($keys as $i => $key) {
+                $result[$key] = $values[$i] ?? $default;
+            }
+            return $result;
+        }, function ($e) use ($keys, $default) {
+            return array_fill(0, count($keys), $default);
+        });
+    }
+
+    public function set($key, $value, $ttl = null)
+    {
+        if ($ttl === null) {
+            return $this->redis->set($key, $value);
+        }
+        return $this->redis->set($key, $value, 'EX', $ttl);
+    }
+
+    public function setMultiple($values, $ttl = null)
+    {
+        $args = [];
+        foreach ($values as $key => $value) {
+            $args[] = $key;
+            $args[] = $value;
+        }
+        if ($ttl === null) {                      
+            return $this->redis->mset(...$args);
+        }
+        $args[] = 'EX';
+        $args[] = $ttl;
+        return $this->redis->mset(...$args);
+
+    }
+
+    public function delete($key)
+    {
+        return $this->redis->del($key);
+    }
+
+    public function deleteMultiple($keys)
+    {
+        return $this->redis->del($keys);
+    }
+
+    public function clear()
+    {
+        // return $this->redis->flushDB();
+    }
+
+    public function has($key)
+    {
+        return $this->redis->exists($key);
+    }
+}
